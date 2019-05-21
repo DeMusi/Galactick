f0e970102c1d2c0b366c31dd00eb42393bd132bc
Mapelisin <36765656+Mapelisin@users.noreply.github.com>
2019-05-21 19:32:07 +0300

    232323

--- Build/Android/src/com/YourCompany/Galactick/OBBData.java
+++ Build/Android/src/com/YourCompany/Galactick/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-23342465L // the length of the file in bytes
+34202514L // the length of the file in bytes
 )
 };
 };
