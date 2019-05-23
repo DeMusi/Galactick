a3d0718311218ef6e7ff38ce1a2ed4d0b1a79e23
KoTeYkA23 <44501531+KoTeYkA23@users.noreply.github.com>
2019-05-23 09:41:33 +0300

    compile for android

--- Build/Android/src/com/YourCompany/Galactick/OBBData.java
+++ Build/Android/src/com/YourCompany/Galactick/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-33554362L // the length of the file in bytes
+20700917L // the length of the file in bytes
 )
 };
 };
